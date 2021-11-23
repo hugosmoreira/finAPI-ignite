c739d0ea022fd8d6e2f2e6cb487ee6347ebc3f66
hugosmoreira <hugo.n4@gmail.com>
2021-11-23 10:40:30 -0800

    finalizado

--- src/server.js
+++ src/server.js
@@ -102,6 +102,53 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send();
 })
 
+
+app.get("statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + "00:00");
+
+  const statement = customer.statement.filter((statement) => statement.createdAt >= dateFormat.toDateString() === new Date(dateFormat).toDateString());
+
+  return response.jason(customer.statement)
+})
+
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+})
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customer)
+})
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement)
+
+  return response.json(balance)
+})
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
